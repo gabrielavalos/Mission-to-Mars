ef42821eec23975776217f3e7cbe3950cdfe3d4b
Gabriela E Avalos <gabrielaeavalos@gabrielas-air.home>
2021-03-23 00:50:32 -0500

    challenge completed

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mission-to-Mars
+##### Created a local webpage using flask to scrape the latest news for Mars.
--- scraping.py
+++ scraping.py
@@ -16,12 +16,12 @@ def scrape_all():
 
     # Run all scraping functions and store results in a dictionary
     data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "hemisphere_imgs": mars_hemisphers(browser),
-        "last_modified": dt.datetime.now(),
+            "news_title": news_title,
+            "news_paragraph": news_paragraph,
+            "featured_image": featured_image(browser),
+            "facts": mars_facts(),
+            "hemispheres": mars_hemisphers(browser),
+            "last_modified": dt.datetime.now(),
         }
     # Stop webdriver and return data
     browser.quit()
